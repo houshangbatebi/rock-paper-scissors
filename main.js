1d8fbc335b02cac078099cde276f51e6cbf16a8a
houshangbatebi <109485943+houshangbatebi@users.noreply.github.com>
2023-11-10 13:00:29 +0330

    Add getComputerChoice function
    This function will randomly return either Rock, Paper or Scissors.

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ function game() {
     let playerSelection
     let computerScore = 0
     let playerScore = 0
+
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice()
+        console.log(computerSelection)
+    }
+}
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    if (randomNumber == 0) return 'rock'
+    else if (randomNumber == 1) return 'paper'
+    else return 'scissors'
 }
